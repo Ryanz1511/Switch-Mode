5c1f411fbc3224df378853d335eadfbbe91b4511
Muhammad Ryan <mhmmdryan2392@gmail.com>
2025-08-24 11:57:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const toggleSwitch = document.querySelector('#theme-toggle');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const currentTheme = localStorage.getItem("theme");
+
 
 if (currentTheme === "dark") {
   document.body.classList.add("dark-mode");
-  toggleSwitch.checked = true;
+  toggleSwitch.checked = true; 
 } else {
-  toggleSwitch.checked = false;
+  document.body.classList.remove("dark-mode");
+  toggleSwitch.checked = false; 
 }
+
 
 toggleSwitch.addEventListener("change", function () {
   toggleSwitch.disabled = true;
   document.body.classList.toggle("dark-mode");
-  localStorage.setItem("theme", toggleSwitch.checked ? "dark" : "light");
+  localStorage.setItem("theme", document.body.classList.contains("dark-mode") ? "dark" : "light");
   setTimeout(() => {
     toggleSwitch.disabled = false;
   }, 1000);
